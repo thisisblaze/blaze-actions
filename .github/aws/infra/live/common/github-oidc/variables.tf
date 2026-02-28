a44b330e2d84e72d671857886298ce90c6f58a8d
Marek Szapiel <marek@thebyte9.com>
2026-02-28 20:14:49 +0000

    chore(infra): bump core terraform modules to v1.50.8

--- .github/aws/infra/live/common/github-oidc/variables.tf
+++
@@ -1,1 +0,0 @@
-# No variables needed currently for hardcoded OIDC
--- .github/aws/infra/live/third-party/acm/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "certificate_arn" {
-  description = "The ARN of the regional ACM certificate (for ALB)"
-  value       = aws_acm_certificate_validation.main.certificate_arn
-}
-
-output "cloudfront_certificate_arn" {
-  description = "The ARN of the CloudFront ACM certificate (us-east-1)"
-  value       = aws_acm_certificate_validation.cloudfront.certificate_arn
-}
