f2a9511ae0014b373918769f4941222965c3a37b
Marek Szapiel <marek@thebyte9.com>
2026-03-15 21:36:16 +0000

    chore: align dev and dev-mini network module refs to v1.55.1

--- .github/aws/infra/live/dev-network/main.tf
+++ .github/aws/infra/live/dev-network/main.tf
@@ -51,7 +51,7 @@ provider "aws" {
 }
 
 module "environment_network" {
-  source = "github.com/thisisblaze/blaze-terraform-infra-core//modules/aws/networking/environment-network?ref=dev"
+  source = "github.com/thisisblaze/blaze-terraform-infra-core//modules/aws/networking/environment-network?ref=v1.55.1"
 
   # Override Frontend Subdomain to be 'frontend-dev'
   frontend_subdomain_override = "frontend-dev"
@@ -238,7 +238,7 @@ module "environment_network" {
 
 
 module "ec2_capacity_provider" {
-  source = "github.com/thisisblaze/blaze-terraform-infra-core//modules/aws/ecs/ec2-capacity-provider?ref=dev"
+  source = "github.com/thisisblaze/blaze-terraform-infra-core//modules/aws/ecs/ec2-capacity-provider?ref=v1.55.1"
   count  = var.enable_ec2 ? 1 : 0
 
   # Identity (context provides label defaults, but these are required)
@@ -269,7 +269,7 @@ module "ec2_capacity_provider" {
 }
 
 module "log_bucket" {
-  source  = "github.com/thisisblaze/blaze-terraform-infra-core//modules/aws/storage/s3?ref=dev"
+  source  = "github.com/thisisblaze/blaze-terraform-infra-core//modules/aws/storage/s3?ref=v1.55.1"
   name    = "logs"
   context = module.environment_network.context
 }
